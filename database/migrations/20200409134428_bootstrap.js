803801275552afeb5b596c5c2ee92134bd72636a
Bilguun <bbg.1015@gmail.com>
2020-04-10 15:46:26 -0500

    added comments,ratings schemas. added tests fro post-model functions

--- database/migrations/20200409134428_bootstrap.js
+++ database/migrations/20200409134428_bootstrap.js
@@ -2,17 +2,47 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', tbl => {
-        tbl.increments();
-        tbl.string('email').notNullable().unique();
-        tbl.string('name').notNullable();
-        tbl.string('github_username');
-        tbl.timestamp('registered_at').defaultTo(knex.fn.now());
-        tbl.timestamp('created_at').defaultTo(knex.fn.now());
-        tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+      tbl.increments();
+      tbl.string('email').notNullable().unique();
+      tbl.string('name').notNullable();
+      tbl.string('github_username');
+      tbl.timestamp('registered_at').defaultTo(knex.fn.now());
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+      tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('posts', tbl => {
+      tbl.increments();
+      tbl.text('title').notNullable();
+      tbl.text('body').notNullable();
+      tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+      tbl.timestamp('posted_at').defaultTo(knex.fn.now());
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+      tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('comments', tbl => {
+      tbl.increments();
+      tbl.text('message').notNullable;
+      tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+      tbl.integer('post_id').unsigned().notNullable().references('id').inTable('posts');
+      tbl.timestamp('commented_at').defaultTo(knex.fn.now());
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+      tbl.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('ratings', tbl => {
+      tbl.increments();
+      tbl.integer('rating');
+      tbl.integer('user_id').unsigned().notNullable().references('id').inTable('users');
+      tbl.integer('rater_id').unsigned().notNullable().references('id').inTable('users');
+      tbl.timestamp('rated_at').defaultTo(knex.fn.now());
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+      tbl.timestamp('updated_at').defaultTo(knex.fn.now());
     })
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('ratings')
+    .dropTableIfExists('comments')
+    .dropTableIfExists('posts')
     .dropTableIfExists('users');
 };
---
+++ database/seeds/04-comments.js
@@ -0,0 +1,13 @@
+const csvFilePath = './data/comments.csv';
+const csv = require('csvtojson');
+exports.seed = async function(knex, Promise){
+    const jsonData = await csv().fromFile(csvFilePath);
+    const chunkSize = 100;
+    return knex.batchInsert('comments', jsonData, chunkSize)
+        .then(function(ids){
+            console.log(ids)
+        })
+        .catch(function(err) {
+            console.log(err)
+        })
+};
