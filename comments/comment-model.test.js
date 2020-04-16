66eb75c2d75576c2f19052232fe34f60a623ab7a
Bilguun <bbg.1015@gmail.com>
2020-04-16 12:06:55 -0500

    added tests to comment-model

---
+++ comments/comment-model.test.js
@@ -0,0 +1,23 @@
+const db = require('../database/db-config.js');
+const Comments = require('./comments-model.js');
+
+
+describe('posts model', () => {
+    // beforeEach(async () => {
+    //     await db('posts').truncate();
+    // });
+    it('should set environment to testing', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+    describe('comment', () => {
+        it('should return comments by post id', async () => {
+            const comments = await Comments.findByPostId(3);
+            expect(comments).toHaveLength(3)
+        });
+
+        it('should return added comment', async () => {
+            const newComment = await Comments.add(1,1,'It is a test message!');
+            expect(newComment['message']).toStrictEqual('It is a test message!');
+        });
+    });
+});
